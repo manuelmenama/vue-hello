0a03abe39ccea93626dbcddcb1eabc4056477ca3
manuelmenama <manuel.mena090589@gmail.com>
2022-11-02 14:42:18 +0100

    added class

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("hello-vue loaded");
+
+const {createApp} = Vue;
+
+createApp ({
+
+  data(){
+
+    return {
+
+      nome: "Manuel",
+      cognome: "Mena",
+      fontWeight: "strong"
+    }
+
+  }
+
+}).mount("#app")
