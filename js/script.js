8717303606443221ec733e2eeee60ee57307f0e0
manuelmenama <manuel.mena090589@gmail.com>
2022-11-02 20:41:58 +0100

    minor changes and comments

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 console.log("hello-vue loaded");
 
+//destrutturo Vue e prendo "create app"
 const {createApp} = Vue;
 
+//in createApp inserisco tutte le mie informazioni
 createApp ({
 
+  //data ritorna quelle che saranno le mie variabili
   data(){
 
     return {
@@ -18,14 +21,14 @@ createApp ({
     }
 
   },
-
+  //nell'oggetto methods inserisco quelle che saranno le mie funzioni
   methods: {
-
+    //utilizzo due toggle inversi per gestire img e testo
     toggleImgTxt() {
       this.toggleImg = !this.toggleImg;
       this.toggleText = !this.toggleText;
     }
 
   }
-
+//in mount inserisco quale sarà l'elemento del dom in cui scriverò le mie istruzioni di vue
 }).mount("#app")
